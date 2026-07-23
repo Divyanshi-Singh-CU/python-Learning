3fe326c4802e0e9efba2c3326bb91da0a286952c
Divyanshi Singh <singh2006divyanshi@gmail.com>
2026-07-23 22:32:43 +0530

    Add data type program
    Add a script to demonstrate data types in Python.

---
+++ Basics/data_type.py
@@ -0,0 +1,15 @@
+num=10
+print("value:",num)
+print("Data type:",type(num))
+
+price=89.9
+print("value:",price)
+print("Data type:",type(price))
+
+name="disha"
+print("value:",name)
+print("Data type:",type(name))
+
+a=True
+print("value:",a)
+print("Data type:",type(a))
