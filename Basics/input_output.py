cc09dad8d4345106883789ef126e41bbf3110197
Divyanshi Singh <singh2006divyanshi@gmail.com>
2026-07-23 22:15:39 +0530

    Add input output program
    Added input for a number and corrected print statement for city.

--- Basics/input_output.py
+++ Basics/input_output.py
@@ -1,7 +1,9 @@
 name =input("enter your name")
 age=input("enter yout age")
 city=input("enter your city")
+num=input("enter any number")
 
 print("Name:",name)
 print("Age:",age)
 Print("City:",city)
+print("Num:",num)
