3643661b4899df28e54f9faadea44c81ad94ae5c
Divyanshi Singh <singh2006divyanshi@gmail.com>
2026-07-23 22:47:43 +0530

    Add data type conversion program

---
+++ Basics/data_type conversion.py
@@ -0,0 +1,39 @@
+#string to integer
+num = "25"
+
+print("Before:", num, type(num))
+
+num = int(num)
+
+print("After:", num, type(num))
+
+#integer to float
+
+num = 10
+
+print("Before:", num, type(num))
+
+num = float(num)
+
+print("After:", num, type(num))
+
+
+#float to int
+
+num = 12.75
+
+print("Before:", num, type(num))
+
+num = int(num)
+
+print("After:", num, type(num))
+
+#user input type conversion
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+sum = a + b
+
+print("Sum =", sum)
+
