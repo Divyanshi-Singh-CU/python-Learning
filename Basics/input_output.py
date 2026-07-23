8d47613d3c4769463956abe36a248449785a8745
Divyanshi Singh <singh2006divyanshi@gmail.com>
2026-07-23 22:27:37 +0530

    Add input output program
    Updated input prompts to include colons for clarity.

--- Basics/input_output.py
+++ Basics/input_output.py
@@ -1,7 +1,7 @@
-name =input("enter your name")
-age=input("enter yout age")
-city=input("enter your city")
-num=input("enter any number")
+name =input("enter your name:")
+age=input("enter yout age:")
+city=input("enter your city:")
+num=input("enter any number:")
 
 print("Name:",name)
 print("Age:",age)
