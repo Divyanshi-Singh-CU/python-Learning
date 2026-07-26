4387323cfcec630ce4f1e4d8fffec3013bfe1d95
Divyanshi Singh <singh2006divyanshi@gmail.com>
2026-07-26 21:26:54 +0530

    Add comparison operators program

---
+++ Basics/Operators/comparison_operators.py
@@ -0,0 +1,12 @@
+a = 20
+b = 5
+
+print("Value of a =", a)
+print("Value of b =", b)
+
+print("a == b :", a == b)
+print("a != b :", a != b)
+print("a > b  :", a > b)
+print("a < b  :", a < b)
+print("a >= b :", a >= b)
+print("a <= b :", a <= b)
