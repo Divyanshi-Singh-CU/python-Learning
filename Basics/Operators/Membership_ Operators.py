7c4f6df3e3bd7845c797e5e4b1521ba1884bd4c0
Divyanshi Singh <singh2006divyanshi@gmail.com>
2026-07-26 21:39:31 +0530

    Add membership operators program
    Demonstrate the use of membership operators 'in' and 'not in' with a list of games.

---
+++ Basics/Operators/Membership_ Operators.py
@@ -0,0 +1,13 @@
+# Membership operators in and not in
+
+Games = ["hockey", "chess", "cricket"]
+
+print("Fruits:", Games)
+
+print("\nUsing 'in' operator:")
+print("chess" in Games)
+print("Ludo" in Games)
+
+print("\nUsing 'not in' operator:")
+print("ludo" not in Games)
+print("cricket" not in Games)
