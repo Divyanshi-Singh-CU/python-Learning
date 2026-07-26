a237f3423f3daa3466e53ed7b7a1a3980513ed5f
Divyanshi Singh <singh2006divyanshi@gmail.com>
2026-07-26 22:11:10 +0530

    Add identity operators program
    Added demonstration of identity operators in Python.

---
+++ Operators/Identity_ operators .py
@@ -0,0 +1,13 @@
+# Identity operators is and is not
+
+
+a = [2, 4, 8]
+b = a
+c = [2, 4, 8]
+
+print("a =", a)
+print("c =", c)
+
+print("\na is b      :", a is b)
+print("a is c      :", a is c)
+print("a is not c  :", a is not c)
